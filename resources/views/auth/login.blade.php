3366b3949b7e1f7bcdedd10fb027d0b3d4a1c8bc
sellyulimaputri <sellyputri3000@gmail.com>
2024-01-21 16:25:17 -0800

    1.2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Batik Kesayangan</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assetss/style.css">
 </head>
 
 <body>
